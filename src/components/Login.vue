48ecbabc66a3e190809b9889dec1ddd7ea697627
victoriaantonijevic <mavickya@gmail.com>
2023-12-02 11:39:20 +0100

    style login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -28,21 +28,20 @@ export default {
 
 <template>
   <div class="row align-items-center">
-    <div class="">
-      <div class="notepaper">
-        <h1></h1>
-        <h2>¡Hello!</h2>
+    <div class="text-center">
+      <img src="/logo.svg" alt="Go For It" class="logo" />
+      <div class="login-dark">
         <form @submit.prevent="login">
-          <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" v-model="email" required>
+          <div class="text-left mb-2">
+            <label for="email">Email</label>
+            <input type="email" id="email" v-model="email" required class="form-control">
           </div>
-          <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" v-model="password" required>
+          <div class="text-left mb-2">
+            <label for="password">Password</label>
+            <input type="password" id="password" v-model="password" required class="form-control">
           </div>
-          <button type="submit">Login</button>
-          <button type="button" @click="$router.push('/register')">Register</button>
+          <button type="submit" class="btn btn-primary btn-block">Login</button>
+          <button type="button" @click="$router.push('/register')" class="btn btn-primary btn-block">Register</button>
         </form>
       </div>
     </div>
@@ -50,7 +49,6 @@ export default {
 </template>
 
 <style scoped>
-
 img {
   width: 300px;
 }
@@ -60,83 +58,70 @@ img {
     width: 600px;
   }
 }
-/*
- * Copyright (c) 2012-2013 Thibaut Courouble
- * http://www.cssflow.com
- * Licensed under the MIT License
- *
- * Sass/SCSS source: https://goo.gl/8hncj
- * PSD by Liam McCabe: https://goo.gl/md3Dj
- */
-
-
-.notepaper {
-  position: relative;
-  margin: 30px auto;
-  padding: 29px 20px 20px 45px;
-  width: 300px;
-  height: 320px;
-  line-height: 30px;
-  color: #6a5f49;
-  text-shadow: 0 1px 1px white;
-  background-color: #f2f6c1;
-  background-image: -webkit-radial-gradient(center, cover, rgba(255, 255, 255, 0.7) 0%, rgba(255, 255, 255, 0.1) 90%), -webkit-repeating-linear-gradient(top, transparent 0%, transparent 29px, rgba(239, 207, 173, 0.7) 29px, rgba(239, 207, 173, 0.7) 30px);
-  background-image: -moz-radial-gradient(center, cover, rgba(255, 255, 255, 0.7) 0%, rgba(255, 255, 255, 0.1) 90%), -moz-repeating-linear-gradient(top, transparent 0%, transparent 29px, rgba(239, 207, 173, 0.7) 29px, rgba(239, 207, 173, 0.7) 30px);
-  background-image: -o-radial-gradient(center, cover, rgba(255, 255, 255, 0.7) 0%, rgba(255, 255, 255, 0.1) 90%), -o-repeating-linear-gradient(top, transparent 0%, transparent 29px, rgba(239, 207, 173, 0.7) 29px, rgba(239, 207, 173, 0.7) 30px);
-  border: 1px solid #c3baaa;
-  border-color: rgba(195, 186, 170, 0.9);
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  -webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.5), inset 0 0 5px #d8e071, 0 0 1px rgba(0, 0, 0, 0.1), 0 2px rgba(0, 0, 0, 0.02);
-  box-shadow: inset 0 1px rgba(255, 255, 255, 0.5), inset 0 0 5px #d8e071, 0 0 1px rgba(0, 0, 0, 0.1), 0 2px rgba(0, 0, 0, 0.02);
+.login-dark{
+  max-width:320px;
+  width:90%;
+  background-color:#262B3E;
+  padding:40px;
+  border-radius:4px;
+  top:50%;
+  left:50%;
+  color:#fff;
+  box-shadow:3px 3px 4px rgba(0,0,0,0.2);
+  margin: auto;
 }
-
-.notepaper:before, .notepaper:after {
-  content: '';
-  position: absolute;
-  top: 0;
-  bottom: 0;
+.login-dark .form-control {
+  background:none;
+  border:none;
+  border-bottom:1px solid #434a52;
+  border-radius:0;
+  box-shadow:none;
+  outline:none;
+  color:inherit;
 }
-
-.notepaper:before {
-  left: 28px;
-  width: 2px;
-  border: solid #efcfad;
-  border-color: rgba(239, 207, 173, 0.9);
-  border-width: 0 1px;
-}
-
-.notepaper:after {
-  z-index: -1;
-  left: 0;
-  right: 0;
-  background: rgba(242, 246, 193, 0.9);
-  border: 1px solid rgba(170, 157, 134, 0.7);
-  -webkit-transform: rotate(2deg);
-  -moz-transform: rotate(2deg);
-  -ms-transform: rotate(2deg);
-  -o-transform: rotate(2deg);
-  transform: rotate(2deg);
-}
-
-label {
-  font-weight: bold;
-  margin-top: 5px;
-}
-input {
-  border: 0;
-  padding: 5px;
-  box-shadow: 5px 5px 5px #6a5f49fb;
-}
-
-button {
-  width: 90px;
-  margin-top: 30px;
+.login-dark .btn-primary {
+  background:#3261FF;
+  border:none;
+  border-radius:4px;
+  padding:11px;
+  box-shadow:none;
+  margin-top:26px;
+  text-shadow:none;
+  outline:none;
   margin-right: 20px;
-  border-radius: 5px;
 }
-
+.login-dark form .btn-primary:hover, .login-dark form .btn-primary:active {
+  background:#214a80;
+  outline:none;
+}
+.login-dark form .forgot {
+  display:block;
+  text-align:center;
+  font-size:12px;
+  color:#6f7a85;
+  opacity:0.9;
+  text-decoration:none;
+}
+.login-dark form .forgot:hover, .login-dark form .forgot:active {
+  opacity:1;
+  text-decoration:none;
+}
+.login-dark .btn-primary:active {
+  transform:translateY(1px);
+}
+.logo{
+  width: 11em;
+  margin: 5em auto 3em auto;
+}
+.text-center{
+  text-align: center;
+}
+.text-left{
+  text-align: left;
+}
+.mb-2{
+  margin-top: 2em;
+}
 </style>
 
 <!-- Imagen de <a href="https://pixabay.com/es/users/peggy_marco-1553824/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1834100">Peggy und Marco Lachmann-Anke</a> en <a href="https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1834100">Pixabay</a> -->
