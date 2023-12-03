5b032c584209be5149212b56984c5337d778a7a6
Edu <eduardoyauri@emmake.com>
2023-12-03 10:46:07 +0100

    Borrar Tasks ok

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -63,6 +63,10 @@ export default {
             this.dueDate = '';
             this.editingTaskId = null;
         },
+
+        eliminarTarea(taskId) {
+        this.userStore.deleteTask(taskId);
+    },
     },
 
     mounted() {
@@ -99,7 +103,7 @@ export default {
                 </select>
               </div>
               <div class="col-xl-3">
-                <button v-on:click="agregarTarea">Add task</button>
+                <button @click="agregarOEditarTarea">Add task</button>
               </div>
             </li>
             <!-- Recorre cada objeto de la colección nombresLista e imprime la tarea -->
@@ -122,7 +126,7 @@ export default {
                 viewBox="0 0 16 16">
                   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                 </svg>
-                <svg 
+                <svg @click="eliminarTarea(task.id)"
                 xmlns="http://www.w3.org/2000/svg" 
                 width="16" height="16" 
                 fill="currentColor" 
