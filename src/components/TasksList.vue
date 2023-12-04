23bed9cbff82cca2adafff167443a0613923b9d8
Sonia Mondéjar <108539030+soniamogo@users.noreply.github.com>
2023-12-04 20:03:29 +0100

    Cambios estilos formulario edición de tarea

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -78,78 +78,86 @@ export default {
 
 
 <template>
-    <div class=" col-xl-10 task-menu">
-          <div>
-            <h2>Task Summary</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="row titulos">
-              <div class="col-6 col-xl-3">TASK NAME</div>
-              <div class="col-xl-3 text-start">TASK DESCRIPTION</div>
-              <div class="col-3 col-xl-2">STATUS</div>
-              <div class="col-3 col-xl-2">ACTIONS</div>
-            </li>
-            <li class="row text-white">
-              <div class="col-6 col-xl-3">
-                <input type="text" v-model="taskName" placeholder="Task" /> <!-- conectamos el input con variable taskName -->
-              </div>
-              <div class="col-xl-3 text-start">
-                <input type="text" v-model="taskDescription" placeholder="Description" /> <!-- conectamos el input con variable taskDescription -->
-              </div>
-              <div class="col-3 col-xl-2">
-                <select class="select-primary-list" v-model="taskStatus"> <!-- conectamos el select con variable taskStatus -->
-                  <option value="onGoing">On Going</option>
-                  <option value="finished">Finished</option>
-                </select>
-              </div>
-              <div class="col-xl-3">
-                <button class="btn-primary-list" @click="agregarOEditarTarea">Add task</button>
-              </div>
-            </li>
-            <!-- Recorre cada objeto de la colección nombresLista e imprime la tarea -->
-            <li
-              v-for="task in userStore.tasks"
-              class="row text-white task-item" 
-              :key="task.id" 
-            >
-              <div class="col-6 col-xl-3">{{ task.text }}</div>
-              <div class="col-xl-3 text-start">{{ task.description }}</div>
-              <div class="col-3 col-xl-2" :class="[!task.completed ? 'onGoing' : 'finished']">
-                {{ formatStatus(task.completed) }}
-              </div>
-              <div class="col-3 col-xl-2">
-                <svg @click="iniciarEdicion(task)"
-                xmlns="http://www.w3.org/2000/svg" 
-                width="16" height="16" 
-                fill="currentColor" 
-                class="bi bi-pencil me-4 ms-2" 
-                viewBox="0 0 16 16">
-                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                </svg>
-                <svg @click="eliminarTarea(task.id)"
-                xmlns="http://www.w3.org/2000/svg" 
-                width="16" height="16" 
-                fill="currentColor" 
-                class="bi bi-trash3" 
-                viewBox="0 0 16 16">
-                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                </svg>
-              </div>
-            <!-- Formulario de Edición -->
-            <div v-if="editingTaskId === task.id" class="edit-form">
+  <div class=" col-xl-10 task-menu">
+    <div>
+      <h2>Task Summary</h2>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="row titulos">
+        <div class="col-6 col-xl-3">TASK NAME</div>
+        <div class="col-xl-3 text-start">TASK DESCRIPTION</div>
+        <div class="col-3 col-xl-2">STATUS</div>
+        <div class="col-3 col-xl-2">ACTIONS</div>
+      </li>
+      <!-- Añadir tarea -->
+      <li class="row text-white">
+        <div class="col-6 col-xl-3">
+          <input type="text" v-model="taskName" placeholder="Task" /> <!-- conectamos el input con variable taskName -->
+        </div>
+        <div class="col-xl-3 text-start">
+          <input type="text" v-model="taskDescription" placeholder="Description" /> <!-- conectamos el input con variable taskDescription -->
+        </div>
+        <div class="col-3 col-xl-2">
+          <select class="select-primary-list" v-model="taskStatus"> <!-- conectamos el select con variable taskStatus -->
+            <option value="onGoing">On Going</option>
+            <option value="finished">Finished</option>
+          </select>
+        </div>
+        <div class="col-3 col-xl-2">
+          <button class="btn-primary-list" @click="agregarOEditarTarea">Add task</button>
+        </div>
+      </li>
+      <!-- Recorre cada objeto de la colección nombresLista e imprime la tarea -->
+      <li
+        v-for="task in userStore.tasks"
+        class="row text-white task-item" 
+        :key="task.id" 
+      >
+        <div class="col-6 col-xl-3">{{ task.text }}</div>
+        <div class="col-xl-3 text-start">{{ task.description }}</div>
+        <div class="col-3 col-xl-2" :class="[!task.completed ? 'onGoing' : 'finished']">
+          {{ formatStatus(task.completed) }}
+        </div>
+        <div class="col-3 col-xl-2">
+          <svg @click="iniciarEdicion(task)"
+          xmlns="http://www.w3.org/2000/svg" 
+          width="16" height="16" 
+          fill="currentColor" 
+          class="bi bi-pencil me-4 ms-2" 
+          viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg>
+          <svg @click="eliminarTarea(task.id)"
+          xmlns="http://www.w3.org/2000/svg" 
+          width="16" height="16" 
+          fill="currentColor" 
+          class="bi bi-trash3" 
+          viewBox="0 0 16 16">
+            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+          </svg>
+        </div>
+        <!-- Formulario de Edición -->
+        <div v-if="editingTaskId === task.id" class="edit-form row mt-4">
+          <div class="col-6 col-xl-3">
             <input type="text" v-model="taskName" placeholder="Task Name" />
+          </div>
+          <div class="col-xl-3 text-start">
             <input type="text" v-model="taskDescription" placeholder="Description" />
-            <select v-model="taskStatus">
+          </div>
+          <div class="col-3 col-xl-2">
+            <select class="select-primary-list" v-model="taskStatus">
                 <option value="onGoing">On Going</option>
                 <option value="finished">Finished</option>
             </select>
+          </div>
             <!-- Añade otros campos si son necesarios -->
-            <button @click="agregarOEditarTarea">Edit</button>
+          <div class="col-3 col-xl-2">
+            <button class="btn-primary-list" @click="agregarOEditarTarea">Edit</button>
+          </div>
         </div>
-            </li>
-            
-          </ul>
-        </div>
+      </li> 
+    </ul>
+  </div>
 </template>
 
 <style scoped>
